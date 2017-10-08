e1c654615d3f37f1c072a97346eaad7564eb3f40
Vladimi Kurguzov <kurguzov74@gmail.com>
2017-10-08 05:28:28 +0300

    2 commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Help-functions :<br />
 
-# ![#c5f015](https://placehold.it/15/c5f015/000000?text=CMD) `БИБЛИОТЕКА ФУНКЦИЙ`               
-# ![#c5f015](https://placehold.it/15/c5f015/000000?text=CMD) `ДЕМО (СНИППЕТЫ)- для наглядности и истории`              
+# ![#c5f015](https://placehold.it/15/c5f015/000000?text='') `БИБЛИОТЕКА ФУНКЦИЙ`               
+# ![#c5f015](https://placehold.it/15/c5f015/000000?text='') `cmd ДЕМО (СНИППЕТЫ)- для наглядности и истории`
+# ![#c5f015](https://placehold.it/15/c5f015/000000?text='') `npm install help-functions`
--- demo/npm/ser.js
+++ demo/npm/ser.js
@@ -1,7 +1,21 @@
 let hf = require('../../index'); //require('help-functions');
 // console.log(gl_hr, '\nВСЕ ФУНКЦИИ В help-functions : \n',  hf, '\n', gl_hr);
+let str = `
+npm help          ПОМОЩЬ
+npm s             ПОИСК МОДУЛЯ 
+npm init          СОЗДАНИЕ ФАЙЛА package.json
+npm i             УСТАНОВИТЬ МОДУЛЬ
 
+npm adduser       ДОБАВИТЬ ПОЛЬЗОВАТЕЛЯ
+npm publish       ОПУБДИКОВАТЬ МОДУЛЬ В npm
+npm unpublish     УДАЛИТЬ МОДУЛЬ
 
+npm r <module>    УДАЛИТЬ МОДУЛЬ У ПОЛЬЗОВАТЕЛЯ (можно и руками)
+npm up            ОБНОВИТЬ МОДУЛЬ У ПОЛЬЗОВАТЕЛЯ
+
+npm login
+npm logout
+`
 console.log('\n\n=====================ОКОНЧАНИЕ БЛОКА NPM=====================') ;
 console.log("console.log(gl_hr, '\\nВСЕ ФУНКЦИИ В help-functions : \\n',  hf, '\\n', gl_hr);");
 
